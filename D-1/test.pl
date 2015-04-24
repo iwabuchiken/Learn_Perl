9852034fb758fc9d450e616313e7852544080255
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-04-24 09:31:32 +0900

    D-1
    v-1.0
    ---
    1. setup: C:\WORKS\Utils\shortcuts\prog_SciLab\start_applications.pl
    
    	==> done

--- D-1/test.pl
+++ D-1/test.pl
@@ -53,6 +53,90 @@ sub test_D_4_V_1_0_Split {
 	
 	printf("[%d] p1 => %s\n", __LINE__, $p1);
 	
+	###############################
+	# tasklist
+	###############################
+	my $list_str = `tasklist|sort`;
+#	my $list_str = `dir`;
+	
+#	printf("[%d] list_str => %s\n", __LINE__, $list_str);
+	
+#	my $p = "(?<=[0-9]+ K)";	#=> no: Variable length lookbehind not implemented in regex m/(?<=[0-9]+ K)/
+	my $p = "(?<=[0-9] K)";	#=> working
+#	my $p = "(?<='\d' K)";	#=> not splitted
+#	my $p = "(?<= K)";
+#	my $p = "(?<=\d+ K)";	#=> Unrecognized escape \d passed through
+#	my $p = " K";		#=> w
+#	my $p = "\d+ K";	#=> n/w
+	
+	my @abc = split /$p/, $list_str;
+	
+	my $count = 1;
+	
+#	my $p3 = /eclipse.exe/;	#=> Use of uninitialized value $_ in pattern match (m//)
+	my $p3 = "eclipse.exe";	#=> 
+
+	my $eclipse_running = 0;
+	
+	foreach my $val (@abc) {
+		
+#    	print "($count) $val\n";
+    	
+    	
+    	if($val =~ /$p3/) {
+#    	if($val =~ $p3) {
+#    	if($val =~ /tasklist/) {
+    		
+	    	printf("[%d] (%d) %s\n", __LINE__, $count, $val);
+#    		$val = "(yes)".$val;
+    		
+#    		my $res = `start C:/WORKS/Programs/scilab-5.5.1/bin/WScilex`;
+    		
+    		$eclipse_running = 1;
+#    		$eclipse_running = true;
+    		
+    	}
+    	
+    	$count ++;
+    	
+  	}
+	
+	if($eclipse_running != 1) {
+#	if($eclipse_running) {
+#	if($eclipse_running == false) {
+		
+		printf("[%d] starting => %s\n", __LINE__, "start_apps");
+		
+		my $res = `start C:/WORKS/Utils/shortcuts/prog_SciLab/start_apps.bat`;
+		
+	} else {
+		
+		printf("[%d] starting => %s\n", __LINE__, "start_apps.no_eclipse");
+		
+		my $res = `start C:/WORKS/Utils/shortcuts/prog_SciLab/start_apps.no_eclipse.bat`;
+		
+	}
+	
+#	print "-- ".$_, "\n" for @abc;
+#	print "-- ", $_, "\n" for @abc;
+	
+#	#REF http://stackoverflow.com/questions/16872340/split-a-string-into-array-in-perl answered Jun 1 '13 at 11:50
+#	my $line = "file1.gzfile2.txtfile2.gzfile3.xls";
+#	my @exts = ('txt');
+##	my @exts = ('txt', 'xls', 'gz');
+#	my $patt = join '|', map { '(?<=\.' . $_ . ')' } @exts;
+#	
+#	printf("[%d] pattern => %s\n", __LINE__, $patt);
+#	
+#	printf("[%d] split => %s\n", __LINE__, $patt.$line);
+#	
+#	
+#	my @abc = split /$patt/, $line;
+#	
+#	print $_, "\n" for @abc;
+	
+	
+	
 }
 
 sub test_D_4_V_1_0 {
@@ -147,13 +231,20 @@ sub test_D_4_V_1_0 {
 
 }
 
-#test_SpecialVars();
+sub do_job {
+	
+	#test_SpecialVars();
+	
+	#REF http://affy.blogspot.jp/p5be/ch05.htm
+#	test_Sub("ab", "bc", "de");
+	#test_Sub("A".."Z");	#=> "26"
+	#test_Sub(1..3);	#=> w
+	
+	test_D_4_V_1_0_Split(10);	#=> w
+	#test_D_4_V_1_0_Split;	#=> w
+	#test_D_4_V_1_0;
+	
+}
 
-#REF http://affy.blogspot.jp/p5be/ch05.htm
-test_Sub("ab", "bc", "de");
-#test_Sub("A".."Z");	#=> "26"
-#test_Sub(1..3);	#=> w
+do_job
 
-test_D_4_V_1_0_Split(10);	#=> w
-#test_D_4_V_1_0_Split;	#=> w
-#test_D_4_V_1_0;
